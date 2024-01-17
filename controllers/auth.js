2e14061445a70d58c2876676cefc1e5dad6c8c6c
AymanElsawy <aymanmoka94@icloud.com>
2024-01-17 20:48:17 +0200

    project init

---
+++ controllers/auth.js
@@ -0,0 +1,7 @@
+export function login(req, res) {
+  console.log(req.body);
+}
+
+export function signup(req, res) {
+  console.log(req.body);
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import authRoute from "./routes/auth.js";
+dotenv.config();
+
+
+const app = express();
+
+app.use(express.urlencoded());
+app.use(express.json());
+
+await mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.error(err));
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.use(process.env.API_VERSION, authRoute);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
