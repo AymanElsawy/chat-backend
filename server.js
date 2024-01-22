568b8cfad6102e474566ecc901011be5f5f8cf58
AymanElsawy <aymanmoka94@icloud.com>
2024-01-22 03:56:49 +0200

    add socket io

--- server.js
+++ server.js
@@ -2,14 +2,28 @@ import express from "express";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
 import cors from 'cors';
+import { Server } from 'socket.io';
+import http from 'http';
+
+import chat from "./socket/chat.js";
+
 import authRoute from "./routes/auth.js";
 import chatRoute from "./routes/chat.js";
 dotenv.config();
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
 app.use(cors());
 
 app.use(express.urlencoded());
 app.use(express.json());
+
+
 
 await mongoose
   .connect(process.env.MONGODB_URL)
@@ -25,6 +39,9 @@ app.get("/", (req, res) => {
 app.use(process.env.API_VERSION, authRoute);
 app.use(process.env.API_VERSION, chatRoute);
 
-app.listen(port, () => {
+chat(io);
+
+
+server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
---
+++ socket/chat.js
@@ -0,0 +1,10 @@
+export default function chat (io){
+    io.on('connection', (socket) => {
+        socket.on('chat message', (msg) => {
+          io.emit('chat message', msg);
+        })
+        socket.on('disconnect', () => {
+          console.log('user disconnected');
+        });
+      });
+}
